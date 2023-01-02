7c40a1250c36ee603e7b87bb587dba49a2f9bdae
nolanzhong <nolan.zhong@duke.edu>
2023-01-02 01:07:57 -0500

    created all error messages

--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -21,11 +21,32 @@ class ViewController: UIViewController {
 
     @IBAction func signUpClicked(_ sender: Any) {
         // creating an alert message
+        /*
         let alert = UIAlertController(title: "Error", message: "Username not found", preferredStyle: UIAlertController.Style.alert)
         let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { UIAlertAction in
             // button clicked what done when clicking
             print("button clicked")
         }
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+         */
+        if usernameText.text == "" {
+            makeAlert(titleInput: "Error", messageInput: "Username Missing")
+        }
+        else if passwordText.text == "" {
+            makeAlert(titleInput: "Error", messageInput: "Password Missing")
+        }
+        else if passwordText.text != password2Text.text {
+            makeAlert(titleInput: "Error", messageInput: "Passwords Different")
+        }
+        else {
+            makeAlert(titleInput: "Success", messageInput: "User OK")
+        }
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
         alert.addAction(okButton)
         self.present(alert, animated: true, completion: nil)
     }
